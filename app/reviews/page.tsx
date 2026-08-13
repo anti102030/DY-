5f96115cb05609c65bbc19d6159eaf50b73f7073
anti102030 <anti102030@gmail.com>
2026-08-13 14:51:39 +0900

    고객후기 워터마크 위치 미세조정

--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -851,7 +851,7 @@ export default async function ReviewsPage({
         .dy-review-watermark-image .dy-review-page-watermark,
         .dy-review-watermark-empty .dy-review-page-watermark {
           position: absolute;
-          top: 45%;
+          top: 40%;
           left: 50%;
           width: 48% !important;
           max-width: 250px !important;
